0900ee6e4580ccdaa8aa6eec172e00bb69301a97
ZakhTania <tatizarova@gmail.com>
2024-06-17 15:10:02 +0300

    common types

---
+++ src/types/types.ts
@@ -0,0 +1,15 @@
+export type FilterType = {
+    sort: string;
+    query: string;
+};
+export type UserType = {
+    login: string;
+    id: number;
+    node_id: string;
+    avatar_url: string;
+    gravatar_id: string;
+    url: string;
+    html_url: string;
+    followers_url: string;
+    suspended_at?: string;
+};
