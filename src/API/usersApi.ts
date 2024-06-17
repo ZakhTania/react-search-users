8d1ff1c8f2d68df136278665142a5323196e135b
ZakhTania <tatizarova@gmail.com>
2024-06-17 14:58:25 +0300

    add search users request

---
+++ src/API/usersApi.ts
@@ -0,0 +1,27 @@
+import { Octokit } from '@octokit/core';
+import { Endpoints } from '@octokit/types';
+
+const octokit = new Octokit({});
+type listUsersParameters = Endpoints['GET /search/users']['parameters'];
+type listUsersResponse = Endpoints['GET /search/users']['response'];
+
+export async function getUsers({
+    q,
+    sort = 'repositories',
+    order,
+    per_page = 30,
+    page = 1
+}: listUsersParameters) {
+
+    const response = await octokit.request(`GET /search/users`, {
+        q,
+        sort,
+        order,
+        per_page,
+        page
+    });
+    
+    const data: listUsersResponse['data'] = await response.data;
+
+    return data;
+}
