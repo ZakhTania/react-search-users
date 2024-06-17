bb26c1e73c4fb994e9f302782453557d4657a57e
ZakhTania <tatizarova@gmail.com>
2024-06-17 08:25:08 +0300

    add GlobalStyle

---
+++ src/Global.styled.ts
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalSlyle = createGlobalStyle`
+
+body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    color: #c5c6c7;
+    font-size: 18px;
+    background-color: #1f2833;
+}
+
+a {
+    font-size: 18px;
+    font-weight: bold;
+    color: #c5c6c7;
+    text-decoration: none;
+}
+
+`;
