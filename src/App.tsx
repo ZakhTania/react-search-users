d9d8b9846bbc292c0efe74d4a3d2f2ff21a05c38
ZakhTania <tatizarova@gmail.com>
2024-06-19 22:03:46 +0300

    change routers

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import { AppRoutes } from './routes/appRouters';
 import UsersPage from './pages/UsersPage';
 import HomePage from './pages/HomePage';
+import UserIdPage from './pages/UserIdPage';
 
 function App() {
     return (
         <Routes>
             <Route path={AppRoutes.HOME} element={<HomePage />} />
             <Route path={AppRoutes.USERS} element={<UsersPage />} />
+            <Route path={AppRoutes.USERS_ID} element={<UserIdPage />} />
         </Routes>
     );
 }
--- src/routes/appRouters.ts
+++ src/routes/appRouters.ts
@@ -2,7 +2,5 @@ export const AppRoutes = {
     HOME: "/",
     USERS: "/users",
     USERS_ID: "/users/:id",
-    ABOUT: "/about",
-    LOGIN: "/login",
     NOT_FOUND: "*",
 }
